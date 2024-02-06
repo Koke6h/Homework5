78f8206ae76bb2b93cb1908d43077c7017d47840
User <git@github.com:Koke6h/helloworld.git>
2024-02-06 18:36:04 +0400

    исправил дз

--- src/Main.java
+++ src/Main.java
@@ -21,19 +21,20 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
         System.out.println("Задача 3");
-        int year = 2024;
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        int year = 1500;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 && year>1584) {
             System.out.println(year + " год является високосным");
-        } else {
+        } else if (year%4!=0 && year %100 ==0 || year% 400 !=0 && year>1584) {
             System.out.println(year + " год не является високосным");
-        }
+        } else {
+            System.out.println("Ошибка");}
         System.out.println("Задача 4");
-        int deliveryDistance = 95;
+        int deliveryDistance = 40;
         int days = 1;
         if (deliveryDistance < 20) {
             System.out.println("Потребуется дней:" + days);
         } else if (deliveryDistance > 20 && deliveryDistance < 60) {
-            System.out.println("Потребуется дней:" + (days++));
+            System.out.println("Потребуется дней:" + (days+1));
         } else if (deliveryDistance > 60 && deliveryDistance < 100) {
             System.out.println("Потребуется дней:"+ (days+2));
     } else{
