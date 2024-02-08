336be9a793d52df66686827dc9f3905afa9e41c9
User <git@github.com:Koke6h/helloworld.git>
2024-02-08 16:18:36 +0400

    исправил дз 2

--- src/Main.java
+++ src/Main.java
@@ -40,24 +40,24 @@ public class Main {
     } else{
         System.out.println("Доставки нет");}
         System.out.println("Задача 5");
-        int monthNumber = 12;
+        int monthNumber = 3;
         switch (monthNumber){
             case 12:
-            case '1':
-            case '2':
+            case 1:
+            case 2:
                 System.out.println("Зима");
                 break;
-            case '3':
-            case '4':
-            case '5':
+            case 3:
+            case 4:
+            case 5:
                 System.out.println("Весна");
                 break;
-            case '6':
-            case '7':
-            case '8':
+            case 6:
+            case 7:
+            case 8:
                 System.out.println("Лето");
                 break;
-            case '9':
+            case 9:
             case 10:
             case 11:
                 System.out.println("Осень");
